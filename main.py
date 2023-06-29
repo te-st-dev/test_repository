bd9fef5c93c3b26275e4660c75273bceab5335f4
te-st-dev <iquququaqu.testdeveloper@gmail.com>
2023-06-29 11:03:02 -0400

    some bugfixes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import file1
 import file2
 
 if __name__ == "__main__":
-    file1.hello_message()
+    #file1.hello_message()
     print("module starts")
     file1.file_updater()
     file2.run_app()
