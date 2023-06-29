3540e5bfeab99bd29c2e1d23031df070482739b4
te-st-dev <iquququaqu.testdeveloper@gmail.com>
2023-06-29 10:32:46 -0400

    some bugfixes

--- file1.py
+++ file1.py
@@ -1,9 +1,16 @@
 import datetime
+import time
 
 def hello_message():
     print("Hello World")
+    infinity_printer()
 
 def file_updater():
     with open("/home/client/program_logs/data.txt", "a") as file:
         formatted_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%s")
         file.write("I updated file at {}\n".format(formatted_datetime))
+
+def infinity_printer():
+    while True:
+        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%s"))
+        time.sleep(4)
